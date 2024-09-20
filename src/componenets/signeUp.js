f53795f12f150bf244427e3627bce10696772a2a
Chakibceran22 <grabachakib008@gmail.com>
2024-09-21 00:05:08 +0100

    modifying signeup logic

--- src/componenets/signeUp.js
+++ src/componenets/signeUp.js
@@ -10,7 +10,7 @@ function SignUp() {
     e.preventDefault();
 
     try {
-      const response = await fetch('react-app-backend-chi.vercel.app', {
+      const response = await fetch('react-app-backend-chi.vercel.app/api/signeup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
